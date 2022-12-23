dc3f790eb03b88f71637cc00de31626b94dbdeb2
johanneshere <119467126+johanneshere@users.noreply.github.com>
2022-12-23 04:29:01 +0100

    Create options.lua

---
+++ nvim/lua/johannes/options.lua
@@ -0,0 +1,26 @@
+vim.cmd([[
+filetype plugin indent on 
+syntax on     
+]])
+
+vim.opt.swapfile = false 
+vim.opt.backup = false 
+vim.opt.writebackup = false 
+vim.opt.undofile = true 
+vim.opt.clipboard = "unnamedplus" 
+
+vim.opt.relativenumber = true 
+vim.opt.nu = true 
+vim.opt.compatible = false 
+vim.opt.shiftwidth = 2  
+vim.opt.tabstop = 2 
+vim.opt.softtabstop = 2 
+vim.opt.expandtab = true 
+vim.opt.autoindent = true 
+vim.opt.smartindent = true 
+vim.opt.wrap = false 
+vim.opt.scrolloff = 10
+vim.opt.termguicolors = true 
+
+vim.opt.errorbells = false 
+vim.opt.incsearch = true 

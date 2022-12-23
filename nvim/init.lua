4eef9c1b509fe923691b6f8eede8d2a204786c3f
johanneshere <119467126+johanneshere@users.noreply.github.com>
2022-12-23 04:28:13 +0100

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+----------------------------------------------------------
+-- I M P O R T S	M O D U L E S 
+----------------------------------------------------------
+
+require "johannes.options"
+require "johannes.plugins"
+require "johannes.colorschemes"
+require "johannes.cmp"
